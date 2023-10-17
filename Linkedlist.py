27a6e9c0db8f4fa48418aa5a080dd949658e7f9b
Srishti2703 <srishtichandra2009@gmail.com>
2023-10-17 11:48:25 +0530

    Create Linkedlist.py

---
+++ Linkedlist.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, data):
+        if self.head is None:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == data:
+                current.next = current.next.next
+                return
+            current = current.next
+
+    def search(self, data):
+        current = self.head
+        while current:
+            if current.data == data:
+                return True
+            current = current.next
+        return False
+
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        return elements
+
+# Example usage:
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
+
+    print("Linked List:", linked_list.display())
+
+    linked_list.prepend(0)
+    linked_list.delete(2)
+    
+    print("Linked List after prepending 0 and deleting 2:", linked_list.display())
+
+    search_result = linked_list.search(3)
+    print("Is 3 in the Linked List? ", search_result)
